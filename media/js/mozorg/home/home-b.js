a2b4306b73ad8e0bafd7f115b0840d1c830f3629
Jon Petto <jonpetto@gmail.com>
2016-06-09 10:32:47 -0500

    Place survey based on prototype. Hide Fx download button for up-to-date users.

--- media/js/mozorg/home/home-b.js
+++ media/js/mozorg/home/home-b.js
@@ -5,11 +5,18 @@
 (function($) {
     'use strict';
 
+    var mozClient = window.Mozilla.Client;
+
     var $toggleInnovate = $('#toggle-innovate');
     var $toggleWho = $('#toggle-who');
     var $whoInnovateWrapper = $('#who-innovate-wrapper');
     var $who = $('#who');
     var $innovate = $('#innovate');
+
+    // hide download button for up-to-date fx desktop users
+    if (mozClient.isFirefoxDesktop && mozClient._isFirefoxUpToDate(false)) {
+        $('#nav-download-firefox').css('display', 'none');
+    }
 
     $toggleWho.on('click', function() {
         $whoInnovateWrapper.toggleClass('open-who');
--- media/js/mozorg/home/survey.js
+++ media/js/mozorg/home/survey.js
@@ -1,7 +1,12 @@
 (function($) {
     'use strict';
 
+    var $body = $('body');
     var $message = $('<div id="survey-message"><span class="survey-invite">Hello! Would you be willing to take a minute to answer a few questions for Mozilla?</span><a class="survey-button" href="https://www.surveygizmo.com/s3/2811071/Help-us-by-sharing-your-feedback-about-Mozilla">Sure. I\'ll help.</a></div>');
 
-    $('main').prepend($message);
+    if ($body.prop('id') === 'home-b') {
+        $body.prepend($message);
+    } else {
+        $('main').prepend($message);
+    }
 })(window.jQuery);
